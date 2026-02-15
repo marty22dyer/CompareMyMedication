8f5eed8819c3a5097ba4bac90570146418a1493a
marty22dyer <marty22dyer@yahoo.com>
2026-02-15 16:40:58 -0700

    Update favicon to use custom pill with dollar sign icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>💊</text></svg>" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
       </head>
       <body>
         <header className="cmm-nav">
