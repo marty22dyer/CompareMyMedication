6f09e2d12e2729ee55ee811227aef7c984607cf3
marty22dyer <marty22dyer@yahoo.com>
2026-02-26 18:34:51 -0700

    Fix www to non-www redirect to resolve 82 duplicate canonical pages in Google Search Console

--- next.config.js
+++ next.config.js
@@ -12,6 +12,16 @@ const nextConfig = {
   generateBuildId: async () => {
     return 'build-' + Date.now()
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.comparemymedication.com' }],
+        destination: 'https://comparemymedication.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
